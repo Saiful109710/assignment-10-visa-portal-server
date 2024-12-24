5c99cfe483274d0d71afc9e8ec6a4be438e45bcf
saiful-islam <saifulislam109710@gmail.com>
2024-12-24 21:29:54 +0600

    add more data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000
 
@@ -27,6 +27,7 @@ app.use(cors())
 async function run(){
     try{
             const visaInfo = client.db('visaInfo').collection('visaType')
+            const visaApplicationData =client.db('visaInfo').collection('visaApplicationData') 
 
             app.post('/allVisa',async(req,res)=>{
                 const data = req.body;
@@ -35,15 +36,34 @@ async function run(){
 
             })
 
+            app.post('/visaApplication',async(req,res)=>{
+                const data = req.body;
+                const result = await visaApplicationData.insertOne(data);
+                res.send(result)
+            })
+
             app.get('/allVisa',async(req,res)=>{
                 const result = await visaInfo.find().toArray();
                 res.send(result)
+            })
+
+            app.get('/allVisa/:id',async(req,res)=>{
+                    const id = req.params.id
+                    const query = {_id:new ObjectId(id)};
+                    const result = await visaInfo.findOne(query);
+                    console.log(result)
+                    res.send(result);
             })
 
             app.get('/latestVisa',async(req,res)=>{
                 const result = await visaInfo.find().limit(6).toArray();
                 res.send(result)
 
+            })
+
+            app.get('/visaApplication',async(req,res)=>{
+                const result = await visaApplicationData.find().toArray();
+                res.send(result);
             })
 
     }catch(err){
